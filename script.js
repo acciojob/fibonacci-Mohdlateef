fb1029524f674815326e8689aeea1206cd755306
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-14 19:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fibonacci(num) {
 // your code here
-	int prev=0
+	let prev=0
 	let next=1;
 	let sum=0;
 	
-	for(int i=1;i<num;i++)
+	for(let i=1;i<num;i++)
 	{
 		sum=next+prev;
 		prev=next;
